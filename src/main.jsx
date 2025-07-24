ebfd9a8c5f72aff3490eecf68769c4b4a11f09b8
bhushanmurkute56 <bhushanmurkute56@gmail.com>
2025-07-24 13:05:25 +0530

    Final Changes Done

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { BrowserRouter, Routes, Route } from 'react-router';
 import "./index.css";
 import Home from "./assets/View/Home"
 import Navbar from './assets/Component/Navbar';
+import HeroSection from './assets/Component/HeroSection';
+import Projects from './assets/Component/Projects';
+import Main from "./assets/Component/Main";
+import Footer from "./assets/Component/Footer";
 
 const root =  createRoot(document.getElementById('root'));
 
@@ -11,6 +15,10 @@ root.render(
   <Routes>
     <Route path='/' element = {<Home />}></Route>
     <Route path='/Navbar' element = {<Navbar />}></Route>
+    <Route path='/MainPage' element = {<Main />}></Route>
+    <Route path='/HeroSection' element = {<HeroSection />}></Route>
+    <Route path='/Projects' element = {<Projects />}></Route>
+    <Route path='/Footer' element = {<Footer />}></Route>
   </Routes>
 </BrowserRouter>
 );
